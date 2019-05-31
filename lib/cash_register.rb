e816bc204ab798acbebe6540e44a07dc7e96f59a
boruch d <dubywider@gmail.com>
2019-05-31 18:55:55 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,8 +12,13 @@ class CashRegister
 def add_item(title,price, quantity = 1 )
   @title = title
   @total += price * quantity 
-  
+  @transactions << price
+    i = quantity
+    until i == 0 do
+      @items << title
+      i -= 1
 end 
+end
 def apply_discount
 end
 end 
